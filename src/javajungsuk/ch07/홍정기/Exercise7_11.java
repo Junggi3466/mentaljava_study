e0cb981d93aaa96593aa5c5db3d71d6a184013f0
Gosoomok <89175795+Junggi3466@users.noreply.github.com>
2023-01-10 00:13:28 +0900

    Add files via upload

---
+++ src/javajungsuk/ch07/홍정기/Exercise7_11.java
@@ -0,0 +1,60 @@
+package ch07;
+class MyTv2{
+    private boolean isPowerOn;
+    private int channel;
+    private int volume;
+
+    int lastchannel= 0;
+
+
+    final int MAX_VOLUME = 100;
+    final int MIN_VOLUME = 0;
+    final int MAX_CHANNEL = 100;
+    final int MIN_CHANNEL = 1;
+
+
+    public boolean isPowerOn() {
+        return isPowerOn;
+    }
+
+    public void setPowerOn(boolean powerOn) {
+        isPowerOn = powerOn;
+    }
+
+    public int getChannel() {
+        return channel;
+
+    }
+
+    public void setChannel(int channel) {
+        this.channel = channel;
+        lastchannel = channel;
+    }
+
+    public int getVolume() {
+        return volume;
+    }
+
+    public void setVolume(int volume) {
+        this.volume = volume;
+    }
+    void gotoPrevChannel(){
+        channel = lastchannel;
+    }
+}
+
+public class Exercise7_11 {
+    public static void main(String[] args) {
+        MyTv2 t = new MyTv2();
+
+        t.setChannel(10);
+        System.out.println("CH:"+t.getChannel());
+        t.setChannel(20);
+        System.out.println("CH:"+t.getChannel());
+        t.gotoPrevChannel();
+        System.out.println("CH:"+t.getChannel());
+        t.gotoPrevChannel();
+        System.out.println("CH:"+t.getChannel());
+
+    }
+}
